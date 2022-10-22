4a69d528e01404367ed3fe5829857fb6273c3071
Jason Chang <jasonchang.ccs@gmail.com>
2022-10-23 00:25:33 +0800

    commit leetcode try

---
+++ try-any/src/main/java/com/jason/leetcode/S3.java
@@ -0,0 +1,35 @@
+package com.jason.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Sliding window, HashTable, String
+ * Sliding window 目前看起來是可以以兩個index匡起來的一個視窗，在這邊就是i, j，以一個for跑i跟j就可以像一個視窗檢視裡面的狀況
+ */
+public class S3 {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)))
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            map.put(s.charAt(i), i);
+            maxLength = Math.max(maxLength, i - j + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        S3 s3 = new S3();
+        String test1 = "abcabcbb";
+        String test2 = "bbbbb";
+        String test3 = "pwwkew";
+        String test4 = "172-3948j-naduh-f6a934jr;lasdfkh1-2893y9ha.sdnvads9ufy-198239ujdianskjdnv-1y34-r98zdsfhpwe8ey-183-49879efhakjdsajd f sdf13098jfojdsvpaiy";
+        System.out.println(s3.lengthOfLongestSubstring(test1));
+        System.out.println(s3.lengthOfLongestSubstring(test2));
+        System.out.println(s3.lengthOfLongestSubstring(test3));
+        System.out.println(s3.lengthOfLongestSubstring(test4));
+
+    }
+}
