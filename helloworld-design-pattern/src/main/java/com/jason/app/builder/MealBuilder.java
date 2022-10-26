84d8dbf244dc3cf92223419b7ba6f3c303ab8593
Jason Chang <jasonchang.ccs@gmail.com>
2022-10-26 23:41:52 +0800

    commit leetcode try

--- helloworld-design-pattern/src/main/java/com/jason/app/builder/MealBuilder.java
+++ helloworld-design-pattern/src/main/java/com/jason/app/builder/MealBuilder.java
@@ -6,19 +6,22 @@ package com.jason.app.builder;
 public class MealBuilder {
     private Meal meal;
 
-    public void buildMainDish(String mainDish){
+    public MealBuilder buildMainDish(String mainDish){
         System.out.println("製作"+ mainDish +"中...");
         this.meal.setMainDish(mainDish);
+        return this;
     }
 
-    public void buildDrink(String drink){
+    public MealBuilder buildDrink(String drink){
         System.out.println("製作"+ drink +"中...");
         this.meal.setDrink(drink);
+        return this;
     }
 
-    public void buildSideDish(String sideDish){
+    public MealBuilder buildSideDish(String sideDish){
         System.out.println("製作"+ sideDish +"中...");
         this.meal.setSideDish(sideDish);
+        return this;
     }
 
     public Meal produceMeal(){
--- helloworld-design-pattern/src/main/java/com/jason/app/builder/Waiter.java
+++ helloworld-design-pattern/src/main/java/com/jason/app/builder/Waiter.java
@@ -7,25 +7,19 @@ public class Waiter {
     private MealBuilder builder = new MealBuilder();
     public Meal getMealA(){
         builder.setMeal(new Meal());
-        builder.buildMainDish("大麥克");
-        builder.buildDrink("中杯可樂");
-        builder.buildSideDish("中暑");
+        builder.buildMainDish("大麥克").buildDrink("中杯可樂").buildSideDish("中暑");
         return builder.produceMeal();
     }
 
     public Meal getMealB(){
         builder.setMeal(new Meal());
-        builder.buildMainDish("麥香魚");
-        builder.buildDrink("中杯可樂");
-        builder.buildSideDish("中暑");
+        builder.buildMainDish("麥香魚").buildDrink("中杯可樂").buildSideDish("中暑");
         return builder.produceMeal();
     }
 
     public Meal getMealC(){
         builder.setMeal(new Meal());
-        builder.buildMainDish("四塊雞塊");
-        builder.buildDrink("小杯可樂");
-        builder.buildSideDish("小暑");
+        builder.buildMainDish("四塊雞塊").buildDrink("小杯可樂").buildSideDish("小暑");
         return builder.produceMeal();
     }
 }
