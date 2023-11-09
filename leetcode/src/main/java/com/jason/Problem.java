889f9e9e62151b5cfdca0b910b5fa8d85b0026fc
Jason Chang <jasonchang.ccs@gmail.com>
2023-11-09 23:35:32 +0800

    commit leetcode

---
+++ leetcode/src/main/java/com/jason/Problem.java
@@ -0,0 +1,27 @@
+package com.jason;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem {
+    public static void main(String[] args) {
+        int[] asdf = {1, 2};
+
+        System.out.println(moveZeros(new int[]{1, 2, 3, 0, 0, 5}));
+    }
+
+    public static List<Integer> moveZeros(int[] numbers) {
+        List<Integer> resultList = new ArrayList<>();
+        List<Integer> zeroList = new ArrayList<>();
+        for (int number : numbers) {
+            if (number != 0) {
+                resultList.add(number);
+            } else {
+                zeroList.add(0);
+            }
+        }
+
+        resultList.addAll(zeroList);
+        return resultList;
+    }
+}
