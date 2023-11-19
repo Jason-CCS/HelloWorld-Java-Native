547556c1d89db94a17b4a414b56eabd6c47310be
Jason Chang <jasonchang.ccs@gmail.com>
2023-11-20 00:12:03 +0800

    commit leetcode

---
+++ leetcode/src/main/java/com/jason/MergeSortedArray.java
@@ -0,0 +1,46 @@
+package com.jason;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] copiedNums1 = new int[m + n];
+        for (int i = 0; i < m; i++) {
+            copiedNums1[i] = nums1[i];
+        }
+        int nums1Pointer = 0, nums2Pointer = 0;
+        int currentIdx = 0;
+
+        while (nums1Pointer <= m - 1 && nums2Pointer <= n - 1) {
+            if (copiedNums1[nums1Pointer] <= nums2[nums2Pointer]) {
+                nums1[currentIdx] = copiedNums1[nums1Pointer];
+                nums1Pointer++;
+            } else {
+                nums1[currentIdx] = nums2[nums2Pointer];
+                nums2Pointer++;
+            }
+            currentIdx++;
+        }
+
+        if (nums1Pointer == m) {
+            int remaining = n - nums2Pointer;
+            for (int i = 0; i < remaining; i++) {
+                nums1[currentIdx + i] = nums2[nums2Pointer + i];
+            }
+        } else {
+            int remaining = m - nums1Pointer;
+            for (int i = 0; i < remaining; i++) {
+                nums1[currentIdx + i] = copiedNums1[nums1Pointer + i];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 5, 7, 100, 0, 0};
+        int[] nums2 = {1, 99};
+        MergeSortedArray m = new MergeSortedArray();
+        m.merge(nums1, 4, nums2, 2);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
