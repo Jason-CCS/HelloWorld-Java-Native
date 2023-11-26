c6ca6a2f1ce8a2897857fc7645b9bab840579557
Jason Chang <jasonchang.ccs@gmail.com>
2023-11-27 00:12:22 +0800

    commit leetcode

---
+++ leetcode/src/main/java/com/jason/ValidPalindrome.java
@@ -0,0 +1,23 @@
+package com.jason;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        String newString = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        int leftPointer = 0;
+        int rightPointer = newString.length() - 1;
+        while (leftPointer < rightPointer) {
+            if (newString.charAt(leftPointer) != newString.charAt(rightPointer))
+                return false;
+            leftPointer++;
+            rightPointer--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(new ValidPalindrome().isPalindrome("race a car"));
+        System.out.println(new ValidPalindrome().isPalindrome(" "));
+
+    }
+}
---
+++ leetcode/src/main/java/com/jason/ValidParentheses.java
@@ -0,0 +1,84 @@
+package com.jason;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        List<Character> closeChars = Arrays.asList(')', ']', '}');
+        Stack closeStack = new Stack();
+        for (char c : s.toCharArray()) {
+            if (!closeChars.contains(c)) {
+                char close = ' ';
+                switch (c) {
+                    case '(':
+                        close = ')';
+                        break;
+                    case '[':
+                        close = ']';
+                        break;
+                    case '{':
+                        close = '}';
+                        break;
+                }
+                closeStack.put(new Elem(close));
+            } else {
+                if (closeStack.isEmpty() || closeStack.take().value != c)
+                    return false;
+            }
+        }
+        return closeStack.isEmpty();
+    }
+
+    public static class Stack {
+        private Elem last;
+
+        public Stack() {
+        }
+
+        public void put(Elem elem) {
+            if (this.last == null) {
+                last = elem;
+            } else {
+                Elem tmp = last;
+                last = elem;
+                last.previous = tmp;
+            }
+        }
+
+        public Elem take() {
+            Elem out = last;
+            last = last.previous;
+            return out;
+        }
+
+        public boolean isEmpty() {
+            return last == null;
+        }
+    }
+
+    public static class Elem {
+        char value;
+        Elem previous;
+
+        public Elem(char val) {
+            this.value = val;
+        }
+    }
+
+    public static void main(String[] args) {
+//        Stack stack = new Stack();
+//        stack.put(new Elem('a'));
+//        stack.put(new Elem('b'));
+//        stack.put(new Elem('c'));
+//
+//        while (!stack.isEmpty()) {
+//            System.out.println(stack.take().value);
+//        }
+
+        System.out.println(new ValidParentheses().isValid("([]{([])})"));
+        System.out.println(new ValidParentheses().isValid("((([)]))"));
+        System.out.println(new ValidParentheses().isValid("("));
+        System.out.println(new ValidParentheses().isValid("}"));
+    }
+}
