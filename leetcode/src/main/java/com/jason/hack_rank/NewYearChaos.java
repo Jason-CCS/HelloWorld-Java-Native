749743052de3466050bbf09456a6cbadd08e5618
Jason Chang <jasonchang.ccs@gmail.com>
2024-03-15 15:43:36 +0800

    commit leetcode

--- leetcode/src/main/java/com/jason/hack_rank/NewYearChaos.java
+++ leetcode/src/main/java/com/jason/hack_rank/NewYearChaos.java
@@ -8,13 +8,15 @@ public class NewYearChaos {
         int bribes = 0;
 
         for (int i = q.size() - 1; i >= 0; i--) {
+            // if the label got from i differentiate the label it originally should be, and result is greater than 2
+            // it means too chaotic.
             if (q.get(i) - (i + 1) > 2) {
                 System.out.println("Too chaotic");
                 return;
             }
 
             for (int j = Math.max(0, q.get(i) - 2); j < i; j++) {
-                if (q.get(j) > q.get(i)) {
+                if (q.get(j) > q.get(i)) { // count the person at label j has been bribed how many times.
                     bribes++;
                 }
             }
