ce582e23e99bd79e8f0c72e890cfb1cd45447ae8
Jason Chang <jasonchang.ccs@gmail.com>
2024-06-09 22:05:46 +0800

    Implement longestPalindrome method in LongestPalindromicSubstring class
    The method for finding the longest palindromic substring within a given string was added to the LongestPalindromicSubstring class. The method now iterates over the string and checks for the longest possible palindrome instead of returning null. A test case was also included in the main function.

--- leetcode/src/main/java/com/jason/dp/LongestPalindromicSubstring.java
+++ leetcode/src/main/java/com/jason/dp/LongestPalindromicSubstring.java
@@ -3,10 +3,48 @@ package com.jason.dp;
 /**
  * Problem: 5
  * Difficulty: Medium
- *
+ * The problem is intended to find the longest palindromic substring within a given string.
  */
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
-        return null;
+        int end = s.length() - 1;
+        int start = 0;
+        int maxLength = 1;
+        int leftMost = end;
+        int pLeft = 0;
+        int pRight = 0;
+        int head, tail;
+
+        while (start < end) {
+            while (start < end) {
+                if (end - start + 1 <= maxLength) break;
+                tail = end;
+                head = start;
+                // check if is palindrome
+                while (head < tail && head < leftMost && s.charAt(head) == s.charAt(tail)) {
+                    head++;
+                    tail--;
+                    if (head >= tail) { // if true, then start~end is palindrome.
+                        if (end - start + 1 > maxLength) {
+                            // update the maxLength
+                            pLeft = start;
+                            pRight = end;
+                            maxLength = end - start + 1;
+                        }
+                        leftMost = start;
+                    }
+                }
+                start++;
+            }
+            end--;
+            start = 0;
+        }
+
+        return s.substring(pLeft, pRight + 1);
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring solution = new LongestPalindromicSubstring();
+        System.out.println(solution.longestPalindrome("cbbd"));
     }
 }
