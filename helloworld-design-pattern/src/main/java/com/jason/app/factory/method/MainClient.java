f89657dbf83bdca21c0db54382a52c2224202fc7
Jason Chang <jasonchang.ccs@gmail.com>
2024-06-20 17:21:37 +0800

    Rename Client.java to MainClient.java
    The filename for the main client class in the factory method design pattern has been refactored from "Client.java" to "MainClient.java". This was done to provide a clearer distinction and understanding of the class's role within the overall program. Additional comments were also added to explain the purpose and principles of the factory design pattern.

--- helloworld-design-pattern/src/main/java/com/jason/app/factory/method/MainClient.java
+++ helloworld-design-pattern/src/main/java/com/jason/app/factory/method/MainClient.java
@@ -7,8 +7,11 @@ import com.jason.app.factory.simple.Adventurer;
  * 模式名：工廠方法模式
  * 模式解說：一個工廠只生產一個產品，子類工廠生產相對應的子類產品
  * 範例解說：弓箭手訓練營，訓練弓箭手，騎士訓練營，訓練騎士
+ * Insight: The purpose of factory design patter is to encapsulate the new() function into implementing child class.
+ * And the interface provide the core logic to do the business. This way can decouple the client logic and implementation logic.
+ * In SOLID principles, the factory is the O. Open/close principle. Open for extending, but close for modification.
  */
-public class Client {
+public class MainClient {
     public static void main(String[] args) {
         ITrainingCamp trainingCamp = new ArcherTrainingCamp();
         Adventurer adventurer = trainingCamp.trainAdventurer();
