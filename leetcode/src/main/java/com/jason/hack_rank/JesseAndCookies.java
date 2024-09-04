116f354f7928ed5d1270bf826e5af860462a1883
Jason Chang <jasonchang.ccs@gmail.com>
2024-09-04 17:14:15 +0800

    Add JesseAndCookies class for cookie sweetness problem
    Introduced the JesseAndCookies class to solve the problem of increasing cookie sweetness using two different approaches: a less efficient O(times*n) method and an optimized O(n*log(n)) method utilizing priority queues. This addition enhances the algorithm set for dealing with minimum sweetness requirements in a list of cookies.

---
+++ leetcode/src/main/java/com/jason/hack_rank/JesseAndCookies.java
@@ -0,0 +1,84 @@
+package com.jason.hack_rank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class JesseAndCookies {
+    public static int times = 0;
+
+    /**
+     * This solution is TLE.
+     * This solution is O(times*n)
+     *
+     * @param k
+     * @param A
+     * @return
+     */
+    public static int cookies1(int k, List<Integer> A) {
+        // Write your code here
+        if (!(A instanceof ArrayList))
+            A = new ArrayList<>(A);
+
+        Integer firstMin = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE;
+        for (Integer curValue : A) {
+            if (curValue < secondMin) {
+                secondMin = curValue;
+                if (secondMin < firstMin) {
+                    int tmp = firstMin;
+                    firstMin = secondMin;
+                    secondMin = tmp;
+                }
+            }
+        }
+        if (firstMin < k) { // want to mix
+            times++;
+            int newCookie = firstMin + 2 * secondMin;
+            A.remove(firstMin);
+            A.remove(secondMin);
+            A.add(newCookie);
+            if (A.size() == 1 && newCookie < k)
+                return -1;
+        } else {
+            return times;
+        }
+
+        return cookies1(k, A);
+    }
+
+    /**
+     * This solution is O(n*log(n)) cause creating the Heap needs O(n*log(n)) time.
+     *
+     * @param k
+     * @param A
+     * @return
+     */
+    public static int cookies(int k, List<Integer> A) {
+        // Write your code here
+        PriorityQueue<Integer> pq = new PriorityQueue<>(A);
+        int times = 0;
+        while (!pq.isEmpty() && pq.peek() < k) {
+            if (pq.size() == 1)
+                return -1;
+            int newCookie = pq.poll() + pq.poll() * 2;
+            times++;
+            pq.offer(newCookie);
+        }
+
+        return times;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(cookies(10, List.of(52, 96, 13, 37)));
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(List.of(2, 7, 3, 6, 4, 6));
+//        System.out.println(pq.poll());
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .toList();
+    }
+}
