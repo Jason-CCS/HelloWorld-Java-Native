545d83fe3e5bdc4ded4063aa2645a47325562b7b
Jason Chang <jasonchang.ccs@gmail.com>
2024-09-09 00:00:08 +0800

    Refactor palindrome index logic to O(n)
    Revised the palindrome index algorithm to improve its efficiency from O(n^2) to O(n). This involved removing redundant checks and simplifying the approach to detect palindromes by skipping at most one character.

--- leetcode/src/main/java/com/jason/hack_rank/PalindromeIndex.java
+++ leetcode/src/main/java/com/jason/hack_rank/PalindromeIndex.java
@@ -1,35 +1,23 @@
 package com.jason.hack_rank;
 
+/**
+ * 這題不錯，原本以為只能O(n^2)，最後還是被我想出O(n)的解.
+ */
 public class PalindromeIndex {
-
     public static int palindromeIndex(String s) {
         // Write your code here
-        int res = -1;
-        int idx = -1;
-//        System.out.println(s);
-        while (idx < s.length()) {
-            if (isPalindrome(s, idx)) {
-//                System.out.println(idx);
-                return idx;
-            }
-            idx++;
-        }
-
-        return res;
+        // filter out all the string that is palindrome already.
+        if (isPalindrome(s))
+            return -1;
+        // s is definitely not a palindrome.
+        return palindromeIndexBySkippingOnce(s);
     }
 
-    public static boolean isPalindrome(String s, int skipIdx) {
+
+    public static boolean isPalindrome(String s) {
         int leftPointer = 0;
         int rightPointer = s.length() - 1;
         while (leftPointer < rightPointer) {
-            if (leftPointer == skipIdx) {
-                leftPointer++;
-                continue;
-            }
-            if (rightPointer == skipIdx) {
-                rightPointer--;
-                continue;
-            }
             if (s.charAt(leftPointer) != s.charAt(rightPointer))
                 return false;
             leftPointer++;
@@ -38,27 +26,44 @@ public class PalindromeIndex {
         return true;
     }
 
-//hgygsvlfcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh
-//fgnfnidynhxebxxxfmxixhsruldhsaobhcggchboashdlurshxixmfxxxbexhnydinfngf
-//bsyhvwfuesumsehmytqioswpcbxyolapfywdxeacyuruybhbwxjmrrmjxwbhbyuruycaexdwyfpaloyxbcpwsoiqtymhesmuseufwvhysb
-//fvyqxqxynewuebtcuqdwyetyqisappmunmnldmkttkmdlnmnumppasiqyteywdquctbeuwenyxqxqyvf
-//mmbiefhflbeckaecprwfgmqlydfroxrblulpasumubqhbvlqpixvvxipqlvbhqbumusaplulbrxorfdylqmgfwrpceakceblfhfeibmm
-//tpqknkmbgasitnwqrqasolmevkasccsakvemlosaqrqwntisagbmknkqpt
-//prcoitfiptvcxroalqmfpnqyhrubxspplrftomfehbbhefmotfrlppsxburhyqnpfmqlaorxcvtpiftiocrp
+    public static int palindromeIndexBySkippingOnce(String s) {
+        int leftPointer = 0;
+        int rightPointer = s.length() - 1;
+        while (leftPointer < rightPointer) {
+            if (s.charAt(leftPointer) != s.charAt(rightPointer)) {
+                break;
+            }
+            leftPointer++;
+            rightPointer--;
+        }
+        if (isPalindrome(s.substring(leftPointer + 1, rightPointer + 1))) {
+            return leftPointer;
+        }
+        if (isPalindrome(s.substring(leftPointer, rightPointer))) {
+            return rightPointer;
+        }
 
-//fgnfnidynhxebxxxfmxixhsruldhsaobhcggchboashdlurshxixmfxxxbexhnydinfngf
+        return -1;
+    }
+
+    //fgnfnidynhxebxxxfmxixhsruldhsaobhcggchboashdlurshxixmfxxxbexhnydinfngf
     public static void main(String[] args) {
 //        System.out.println(palindromeIndex("a"));
 //        System.out.println(palindromeIndex("aa"));
 //        System.out.println(palindromeIndex("aaa"));
-        System.out.println(isPalindrome("hgygsvlfcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh", -1));
-        System.out.println(isPalindrome("fgnfnidynhxebxxxfmxixhsruldhsaobhcggchboashdlurshxixmfxxxbexhnydinfngf", -1));
-        System.out.println(isPalindrome("bsyhvwfuesumsehmytqioswpcbxyolapfywdxeacyuruybhbwxjmrrmjxwbhbyuruycaexdwyfpaloyxbcpwsoiqtymhesmuseufwvhysb", -1));
-        System.out.println(isPalindrome("fvyqxqxynewuebtcuqdwyetyqisappmunmnldmkttkmdlnmnumppasiqyteywdquctbeuwenyxqxqyvf", -1));
-        System.out.println(isPalindrome("mmbiefhflbeckaecprwfgmqlydfroxrblulpasumubqhbvlqpixvvxipqlvbhqbumusaplulbrxorfdylqmgfwrpceakceblfhfeibmm", -1));
-        System.out.println(isPalindrome("tpqknkmbgasitnwqrqasolmevkasccsakvemlosaqrqwntisagbmknkqpt", -1));
-        System.out.println(isPalindrome("prcoitfiptvcxroalqmfpnqyhrubxspplrftomfehbbhefmotfrlppsxburhyqnpfmqlaorxcvtpiftiocrp", -1));
-        System.out.println(isPalindrome("kjowoemiduaaxasnqghxbxkiccikxbxhgqnsaxaaudimeowojk", -1));
+//        System.out.println(palindromeIndex("abaa"));
+//        System.out.println(palindromeIndex("aaba"));
+//        System.out.println(palindromeIndex("abaaa"));
+//        System.out.println(palindromeIndex("aaaba"));
+//        System.out.println(palindromeIndex("azabbahhh"));
+        System.out.println(palindromeIndex("hgygsvlfcwns1wtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh"));
+        System.out.println(palindromeIndex("fgnfnidynhxebxxxfmxixhsruldhsaobhcggchboashdlurs1hxixmfxxxbexhnydinfngf"));
+        System.out.println(palindromeIndex("bsyhvwfuesumsehmytqioswpcbxyolapfywdxeacyuruybhbwxjmrrmjxwbhbyuruycaexdwyfpaloyxbcpwsoi1qtymhesmuseufwvhysb"));
+        System.out.println(palindromeIndex("fvyqxqxynewuebtcuqdwyetyqisappmunmnldmkttkmdlnmnumppasiqyteywdquctbeuwenyxqxqy1vf"));
+        System.out.println(palindromeIndex("mmbiefhflbeckaecprwfgmqlydfroxrblulpasumubqhbvlqpixvvxipqlvbhqbumusaplulbrxorfdylqmgfwrpceakceblfhfeib1mm"));
+        System.out.println(palindromeIndex("tpqknkmbgasitnwqrqasolmevkasccsakvemlosaqrqwntisagbmknk1qpt"));
+        System.out.println(palindromeIndex("prcoitfiptvcxroalqmfpnqyhrubxspplrftomfehbbhefmotfrlppsxburhyqnpfmqlaorxcvtpiftio1crp"));
+        System.out.println(palindromeIndex("kjowoemiduaaxasnqghxbxkiccikxbxhgqnsaxaaudimeowo1jk"));
     }
 
 }
